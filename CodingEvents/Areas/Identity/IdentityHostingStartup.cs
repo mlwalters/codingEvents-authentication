b989bd3128b94d7eb1b49403f7fdde25c4d6dd17
Maricar Walters <walterskai7@gmail.com>
2021-02-15 18:21:27 -0600

    Updated identityhostingstartup and startup.cs

--- CodingEvents/Areas/Identity/IdentityHostingStartup.cs
+++ CodingEvents/Areas/Identity/IdentityHostingStartup.cs
@@ -1,5 +1,5 @@
 using System;
-using CodingEvents.Areas.Identity.Data;
+using CodingEvents.Data;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Identity.UI;
